9d202f5f34246dbff8b8f26c5c90e85b4bd75cf9
kirirotha <kirirotha@gmail.com>
2020-09-14 17:19:32 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,36 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    increment = () => {
+        const newTimerCount = this.state.secondsLeft - 1
+        this.setState({
+            secondsLeft: newTimerCount
+        })
+    }
+    
+    render() {
+        let bombText
+        if(this.state.secondsLeft > 0){
+            bombText = `${this.state.secondsLeft} seconds left before I go boom!`
+        }else{
+            bombText = "Boom!"
+        }
+
+      return (
+        <div>
+            <h1>{bombText}</h1>
+        </div>
+
+
+      )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,30 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+
+export default class ImageSlider extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    
+    increment = () => {
+        const newSlideIndex = this.state.currentSlideIndex + 1
+        this.setState({
+            currentSlideIndex: newSlideIndex
+        })
+    }
+    
+    render() {
+        
+      return (
+        <div onClick={this.increment}>
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        </div>
+
+      )
+    }
+}
